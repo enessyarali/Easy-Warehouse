8b8df1e4adfb608b09532977cee256bcf56d95f4
ILARIA PILO s296847 <s296847@studenti.polito.it>
2022-05-06 20:01:17 +0200

    Add /api/sku/:id/position implementation

--- code/server/api/skuApi.js
+++ code/server/api/skuApi.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const SKU = require('../model/sku.js')
 const SkuDBU = require('../database_utilities/skuDBU.js')
+const Error = require('../model/error')
 
 let router = express.Router()
 
@@ -58,9 +59,48 @@ router.put('/api/sku/:id', async (req,res) => {
         return res.status(404).json({error: `No SKU matches the id ${req.params.id}.`});
       }
       try {
-        sku.modify(req.body.newDescription, req.body.newWeight, req.body.newVolume, req.body.newNotes, req.body.newPrice, req.body.newAvailableQuantity)
+        sku.modify(db.db, req.body.newDescription, req.body.newWeight, req.body.newVolume, req.body.newNotes, req.body.newPrice, req.body.newAvailableQuantity)
       } catch (err) {
-        return res.status(422).json({error: `The assigned position cannot store the SKU anymore. Update aborted.`});
+        if(err.code===4)
+          return res.status(422).json({error: `The assigned position cannot store the SKU anymore. Update aborted.`});
+        else return res.status(503).json({error: `Something went wrong...`, message: err.message});
+      }
+      await db.updateSKU(sku);
+      return res.status(200).end();
+  }
+  catch(err){
+    return res.status(503).json({error: `Something went wrong...`, message: err.message});
+  }
+});
+
+// PUT /api/sku/:id/position
+// assign a position to a sku
+router.put('/api/sku/:id/position', async (req,res) => {
+  if (req.body === undefined || req.body.position === undefined || req.params === undefined || req.params.id === undefined) {
+    return res.status(422).json({error: `Invalid data.`});
+  }
+  try{
+      const db = new SkuDBU('ezwh.db');
+      // see if the position is already assigned to another SKU
+      const isBusy = await db.searchAssignedPosition(req.body.position);
+      if (isBusy) {
+        return res.status(422).json({error: 'The provided position is already assigned to another SKU. Update aborted.'});
+      }
+      // get the sku to be modified
+      const skuList = await db.loadSKU(req.params.id);
+      const sku = skuList.pop();
+      if (!sku) {
+        // then the inserted id did not match any sku
+        return res.status(404).json({error: `No SKU matches the id ${req.params.id}.`});
+      }
+      try {
+        await sku.setPosition(db.db, req.body.position);
+      } catch (err) {
+        if (err.code==4)
+          return res.status(422).json({error: `Position cannot store the required SKU. Operation aborted.`});
+        if (err.code==5)
+          return res.status(404).json({error: `No position matches the id ${req.body.position}.`});
+        else return res.status(503).json({error: `Something went wrong...`, message: err.message});
       }
       await db.updateSKU(sku);
       return res.status(200).end();
@@ -86,7 +126,7 @@ router.delete('/api/sku/:id', async (req,res) => {
         // then the inserted id did not match any sku
         return res.status(404).json({error: `No SKU matches the id ${req.params.id}.`});
       }
-      sku.delete();
+      sku.delete(db.db);
       // now, delete the sku
       await db.deleteSKU(req.params.id);
       return res.status(204).end();
