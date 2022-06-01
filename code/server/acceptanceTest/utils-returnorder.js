06f5348015db247ce493682856ea9dcdd56fb682
ILARIA PILO s296847 <s296847@studenti.polito.it>
2022-06-01 15:39:56 +0200

    Write correct deleteAll for missing classes

--- code/server/acceptanceTest/utils-returnorder.js
+++ code/server/acceptanceTest/utils-returnorder.js
@@ -26,7 +26,7 @@ function newProductwithRFID(product, rfid){
 //                                          TEST ACTIONS
 //---------------------------------------------------------------------------------------------------------
 
-function deleteAllReturnOrders(agent){
+/*function deleteAllReturnOrders(agent){
     describe('Removing all return orders', function(){
         it('Getting and removing', function(done){
             agent.get('/api/returnOrders')
@@ -42,6 +42,21 @@ function deleteAllReturnOrders(agent){
                 }
                 done();
             }).catch(err=>done(err));
+        });
+    });
+}*/
+function deleteAllReturnOrders(agent){
+    describe('Removing all return orders', function(){
+        it('Getting and removing', async function(){
+            const res = await agent.get('/api/returnOrders');
+            res.should.have.status(200);
+            if(res.body.length !==0){
+                let res2;
+                for( let i=0; i< res.body.length; i++){
+                    res2 = await agent.delete('/api/returnOrder/'+res.body[i].id);
+                    res2.should.have.status(204);
+                }
+            }
         });
     });
 }
